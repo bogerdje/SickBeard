ae3e2add4c3f1f94fd8fcac09ce7112bb49fe243
rintje <rintjerintje@gmail.com>
2011-10-15 15:14:17 +0200

    Removed temporary github testline
    Revert "New branch created for an attempt to improve the reliability of the BierDopje subliminal plugin."
    
    This reverts commit a1a80783d0e080d61def8b92ef860079a744df97.

--- lib/subliminal/subliminal.py
+++ lib/subliminal/subliminal.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the Lesser GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-BREAKING THE BUILD AS A TEST - REMOVE ME!
 
 import threading
 from itertools import groupby
